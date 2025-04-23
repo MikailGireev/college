9366fbe3a672763211bd679d40ccf90c249334c6
Mikail <mikaildevelop@gmail.com>
2025-04-23 12:08:37 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import svgLoader from 'vite-svg-loader';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/colege/',
   plugins: [vue(), vueDevTools(), svgLoader()],
   resolve: {
     alias: {
